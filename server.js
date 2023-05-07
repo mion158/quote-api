5dc992bbef06813e115ffcd5d08ed55680f4421e
mion158 <111487792+mion158@users.noreply.github.com>
2023-05-07 22:47:05 +0100

    downloaded dependencies and set listening port

--- server.js
+++ server.js
@@ -6,5 +6,10 @@ const { getRandomElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
+
 app.use(express.static('public'));
 
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+  
